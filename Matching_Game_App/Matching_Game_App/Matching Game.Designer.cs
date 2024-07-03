4acb2a384000801cc3f80a7209afb391d2d44d5c
yaakoveidelman <yeidelman@eidelmans.com>
2024-07-03 10:32:40 -0400

    Added_the_startup_files

---
+++ Matching_Game_App/Matching_Game_App/Matching Game.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Matching_Game_App
+{
+    partial class Matching_Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Matching_Game
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1482, 843);
+            Name = "Matching_Game";
+            Text = "Matching_Game";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
