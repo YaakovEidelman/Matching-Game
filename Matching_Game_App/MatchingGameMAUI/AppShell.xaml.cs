a2a6f8c3c5fa095d25bcc8726ab3a2af451435ee
yaakoveidelman <yeidelman@eidelmans.com>
2024-10-12 23:07:54 -0400

    #56

---
+++ Matching_Game_App/MatchingGameMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchingGameMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Matching_Game_App/MatchingGameMAUI/MatchingGame.xaml.cs
@@ -0,0 +1,9 @@
+namespace MatchingGameMAUI;
+
+public partial class MatchingGame : ContentPage
+{
+	public MatchingGame()
+	{
+		InitializeComponent();
+	}
+}
