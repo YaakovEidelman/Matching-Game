19ecb40dd230ae7909dc20f712c839ae50019cde
yaakoveidelman <yeidelman@eidelmans.com>
2025-02-03 15:25:35 -0500

    Jquery Update

---
+++ Matching_Game_App/MatchingGameWeb/Pages/Game JQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MatchingGameWeb.Pages
+{
+    public class Game_JQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
